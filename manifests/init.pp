9109b7510b40a584b5bfa9f03c3a05cb072be777
Samuli Seppänen <samuli.seppanen@gmail.com>
2018-06-07 12:55:59 +0300

    Add configuration support
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,34 @@
 #
 # == Class: win32_openssh
 #
-# Install and configure win32-openssh
+# Install and configure win32-openssh (ssh client and server)
+#
+# == Paramemeters
+#
+# [*ensure*]
+#   Status of win32-openssh on the system. Valid values are 'present' (default) 
+#   and 'absent'.
+# [*listenaddress*]
+#   Local IP-addresses sshd binds to. This can be an string containing one bind
+#   address or an array containing one or more. Defaults to "0.0.0.0" (all
+#   IPv4 interfaces).
+# [*port*]
+#   Port on which sshd listens on. Defaults to 22.
+# [*permitrootlogin*]
+#   Allow root logins (yes/no/without-password). Defaults to "yes".
+# [*passwordauthentication*]
+#   Allow logins using password (yes/no). Defaults to "yes".
 #
 class win32_openssh
 (
-    Enum['present','absent'] $ensure = 'present'
+    Enum['present','absent']            $ensure = 'present',
+    Variant[String,Array[String]]       $listenaddress = '0.0.0.0',
+    Integer[1,65535]                    $port = 22,
+    Enum['yes','no','without-password'] $permitrootlogin = 'yes',
+    Enum['yes','no']                    $passwordauthentication = 'yes'
 )
 {
+    $listenaddresses = any2array($listenaddress)
     $install_options = ['--params="/SSHServerFeature"']
 
     package { 'openssh':
@@ -15,13 +36,21 @@ class win32_openssh
         provider          => 'chocolatey',
         install_options   => $install_options,
         uninstall_options => $install_options,
+        require           => Class['::chocolatey'],
+    }
+
+    file { 'sshd_config':
+        ensure  => $ensure,
+        name    => 'C:/ProgramData/ssh/sshd_config',
+        content => template('win32_openssh/sshd_config.erb'),
+        require => Package['openssh'],
     }
 
     if $ensure == 'present' {
         service { 'sshd':
             ensure  => 'running',
             enable  => true,
-            require => Package['openssh'],
+            require => File['sshd_config'],
         }
     }
 }
