9b3b52fe1c28bc80f573cb86567f09f9fa71d053
Samuli Seppänen <samuli.seppanen@gmail.com>
2018-06-07 20:24:26 +0300

    Add support for configuring Windows Firewall
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,9 @@
 #
 # == Paramemeters
 #
+# [*manage_packetfilter*]
+#   Whether to open port for SSH connections in the Windows Firewall or not. 
+#   Defaults to true.
 # [*ensure*]
 #   Status of win32-openssh on the system. Valid values are 'present' (default) 
 #   and 'absent'.
@@ -14,6 +17,9 @@
 #   IPv4 interfaces).
 # [*port*]
 #   Port on which sshd listens on. Defaults to 22.
+# [*allow_address_ipv4*]
+#   IP address(es) or network(s) to allow SSH connections from (string or 
+#   array, default: '127.0.0.1'). Modifies Windows Firewall settings.
 # [*permitrootlogin*]
 #   Allow root logins (yes/no/without-password). Defaults to "yes".
 # [*passwordauthentication*]
@@ -27,9 +33,11 @@
 #
 class win32_openssh
 (
+    Boolean                             $manage_packetfilter = true,
     Enum['present','absent']            $ensure = 'present',
     Variant[String,Array[String]]       $listenaddress = '0.0.0.0',
     Integer[1,65535]                    $port = 22,
+    Variant[String,Array[String]]       $allow_address_ipv4 = '127.0.0.1',
     Enum['yes','no','without-password'] $permitrootlogin = 'yes',
     Enum['yes','no']                    $passwordauthentication = 'yes',
     Boolean                             $disable_microsoft_ssh_server = true,
@@ -79,6 +87,23 @@ class win32_openssh
             ensure  => 'running',
             enable  => true,
             require => File['sshd_config'],
+        }
+    }
+
+    if $manage_packetfilter {
+
+        $allow_address_ipv4_array = any2array($allow_address_ipv4)
+        $remote_ips = join($allow_address_ipv4_array, ',')
+
+        ::windows_firewall::exception { "SSH-in from ${remote_ips} (puppet)":
+            ensure      => 'present',
+            description => "Allow SSH connections from ${remote_ips} to tcp port ${port}",
+            direction   => 'in',
+            action      => 'Allow',
+            enabled     => 'yes',
+            protocol    => 'TCP',
+            local_port  => String($port),
+            remote_ip   => $remote_ips,
         }
     }
 }
